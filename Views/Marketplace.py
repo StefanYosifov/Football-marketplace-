5ccf1851fd0a3e953306c27695045ee3150ee4c7
Ivan <mazdt12@gmail.com>
2023-05-27 21:42:16 +0300

    Added the listbox function

--- Views/Marketplace.py
+++ Views/Marketplace.py
@@ -16,13 +16,23 @@ def openMarketPlace(root):
     label = Label(new_window, text="This is a new window")
     label.pack()
 
-    GListBox_710=tk.Listbox(new_window)  # Use new_window instead of root
+    GListBox_710 = tk.Listbox(new_window)  # Use new_window instead of root
     GListBox_710["borderwidth"] = "1px"
     ft = tkFont.Font(family='Times', size=10)
     GListBox_710["font"] = ft
     GListBox_710["fg"] = "#333333"
     GListBox_710["justify"] = "center"
     GListBox_710.place(x=10, y=40, width=150, height=40)
+
+    def readFromFile(filename):
+        with open(filename, "r") as file:
+            footballers = file.readlines()
+        return footballers
+
+    Footballers = readFromFile("Marketplace.txt")
+
+    for footballer in Footballers:
+        GListBox_710.insert(tk.END, footballer.strip())
 
     GLabel_887 = tk.Label(new_window)  # Use new_window instead of root
     ft = tkFont.Font(family='Times', size=10)
@@ -71,3 +81,8 @@ def openMarketPlace(root):
     GLabel_751["justify"] = "center"
     GLabel_751["text"] = "label"
     GLabel_751.place(x=510, y=40, width=70, height=40)
+
+# Create the main window
+root = tk.Tk()
+openMarketPlace(root)
+root.mainloop()
