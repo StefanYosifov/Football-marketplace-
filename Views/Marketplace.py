9fe191fe587dc5996a54e778fc904d481350857b
Ivan <mazdt12@gmail.com>
2023-05-27 22:26:29 +0300

    added new button for done and listbox for sell/buy

--- Views/Marketplace.py
+++ Views/Marketplace.py
@@ -16,13 +16,14 @@ def openMarketPlace(root):
     label = Label(new_window, text="This is a new window")
     label.pack()
 
-    GListBox_710 = tk.Listbox(new_window)  # Use new_window instead of root
+    GListBox_710=tk.Listbox(root)
     GListBox_710["borderwidth"] = "1px"
-    ft = tkFont.Font(family='Times', size=10)
+    ft = tkFont.Font(family='Times',size=10)
     GListBox_710["font"] = ft
     GListBox_710["fg"] = "#333333"
     GListBox_710["justify"] = "center"
-    GListBox_710.place(x=10, y=40, width=150, height=40)
+    GListBox_710.place(x=10,y=150,width=150,height=40)
+
 
     def readFromFile(filename):
         with open(filename, "r") as file:
@@ -34,55 +35,77 @@ def openMarketPlace(root):
     for footballer in Footballers:
         GListBox_710.insert(tk.END, footballer.strip())
 
-    GLabel_887 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+    GLabel_887=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_887["font"] = ft
     GLabel_887["fg"] = "#333333"
     GLabel_887["justify"] = "center"
     GLabel_887["text"] = "label"
-    GLabel_887.place(x=160, y=40, width=70, height=40)
+    GLabel_887.place(x=160,y=150,width=70,height=40)
 
-    GLabel_234 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+    GLabel_234=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_234["font"] = ft
     GLabel_234["fg"] = "#333333"
     GLabel_234["justify"] = "center"
     GLabel_234["text"] = "label"
-    GLabel_234.place(x=230, y=40, width=70, height=40)
+    GLabel_234.place(x=230,y=150,width=70,height=40)
 
-    GLabel_791 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+
+    GLabel_791=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_791["font"] = ft
     GLabel_791["fg"] = "#333333"
     GLabel_791["justify"] = "center"
     GLabel_791["text"] = "label"
-    GLabel_791.place(x=300, y=40, width=70, height=40)
+    GLabel_791.place(x=300,y=150,width=70,height=40)
 
-    GLabel_973 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+
+    GLabel_973=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_973["font"] = ft
     GLabel_973["fg"] = "#333333"
     GLabel_973["justify"] = "center"
     GLabel_973["text"] = "label"
-    GLabel_973.place(x=370, y=40, width=70, height=40)
+    GLabel_973.place(x=370,y=150,width=70,height=40)
 
-    GLabel_512 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+    GLabel_512=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_512["font"] = ft
     GLabel_512["fg"] = "#333333"
     GLabel_512["justify"] = "center"
     GLabel_512["text"] = "label"
-    GLabel_512.place(x=440, y=40, width=70, height=40)
+    GLabel_512.place(x=440,y=150,width=70,height=40)
 
-    GLabel_751 = tk.Label(new_window)  # Use new_window instead of root
-    ft = tkFont.Font(family='Times', size=10)
+
+    GLabel_751=tk.Label(new_window)
+    ft = tkFont.Font(family='Times',size=10)
     GLabel_751["font"] = ft
     GLabel_751["fg"] = "#333333"
     GLabel_751["justify"] = "center"
     GLabel_751["text"] = "label"
-    GLabel_751.place(x=510, y=40, width=70, height=40)
+    GLabel_751.place(x=510,y=150,width=70,height=40)
 
-# Create the main window
+    GListBox_130=tk.Listbox(new_window)
+    GListBox_130["borderwidth"] = "1px"
+    ft = tkFont.Font(family='Times',size=10)
+    GListBox_130["font"] = ft
+    GListBox_130["fg"] = "#333333"
+    GListBox_130["justify"] = "center"
+    GListBox_130.place(x=240,y=20,width=120,height=70)
+
+    GButton_198=tk.Button(new_window)
+    GButton_198["bg"] = "#f0f0f0"
+    ft = tkFont.Font(family='Times',size=10)
+    GButton_198["font"] = ft
+    GButton_198["fg"] = "#000000"
+    GButton_198["justify"] = "center"
+    GButton_198["text"] = "Button"
+    GButton_198.place(x=240,y=320,width=120,height=70)
+    GButton_198["command"] = self.GButton_198_command
+
+    def GButton_198_command(self):
+        print("command")
 root = tk.Tk()
 openMarketPlace(root)
 root.mainloop()
